342e9d92449bf3b2142146969ac0edf9b0cb3a28
Ron van Bree <ronvbree@gmail.com>
2017-03-06 19:40:41 +0100

    removed commented code

--- I/test_values_tie.py
+++ I/test_values_tie.py
@@ -2,28 +2,6 @@ from I.elementary_automata_values import Rule, State
 from PIL import Image
 import random as rnd
 
-# values = range(4)
-# size = 301
-# mid = int(size/2)
-#
-# r = Rule(values)
-# r.randomize()
-#
-# s = State(size, values)
-# s.set(int((len(s) - 1) / 2), 1)
-
-# Print data
-# print(r)
-# print()
-# print(s)
-
-# Keep track of states
-# states = list()
-# iterations = mid
-# for i in range(iterations):
-#     states.append(s)
-#     s = r.next(s)
-
 
 class Tie:
 
@@ -94,62 +72,3 @@ class Tie:
 tie = Tie(200, 3)
 tie.save("tie.png")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# width = 31
-#
-# bottom_part_length = int(width/2)
-#
-# for i in range(bottom_part_length):
-#     for j in range(mid - i):
-#         states[i].set(j, 0)
-#     for j in range(mid + i, size):
-#         states[i].set(j, 0)
-#
-# offset = bottom_part_length
-# counter = 0
-# for i in range(bottom_part_length, iterations):
-#     for j in range(mid - offset):
-#         states[i].set(j, 0)
-#     for j in range(mid + offset, size):
-#         states[i].set(j, 0)
-#     if counter % int(iterations/16) == 0:
-#         offset -= 1
-#     counter += 1
-#
-# states.reverse()
-#
-# # Visualize states
-#
-# white = (255, 255, 255)
-#
-# color_map = [0] * (max(values) + 1)  # TODO jezus wat lelijk
-# rgb = range(255)
-# for value in values:
-#     color_map[value] = (rnd.choice(rgb), rnd.choice(rgb), rnd.choice(rgb))
-#
-#
-# # color_map[0] = (255, 255, 255)
-# # color_map[1] = (255, 0, 0)
-# # color_map[2] = (0, 255, 0)
-# # color_map[3] = (0, 0, 255)
-#
-# img = Image.new('RGB', (size, iterations), 'black')
-# pixels = img.load()
-# for state in range(iterations):
-#     for i in range(size):
-#         pixels[i, state] = color_map[states[state].get(i)]
-#
-# img.save("tie.png")
